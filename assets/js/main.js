36f0b7435a5b716274cb02ac8acd63f5ee0d4c93
gremzlya <sinelnikovwork@gmail.com>
2013-09-10 15:40:50 -0700

    added backbone etc.

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    var Main = Backbone.View.extend({
+            tagName:"div",
+            className:"container",
+
+            events:{
+                "click input[type='submit']":"sendData",
+            },
+
+            initialize:function () {
+
+            },
+
+            sendData: function ( ) {
+                console.log('data');
+                var data = {};
+
+                $('.words-input input[type=text]').each(function(key, item){ 
+                    data[item.name] = item.value; 
+                });
+                
+                $.ajax({
+                    type: "GET",
+                    url: "/words/create",
+                    data: data
+                }).done(function( msg ) {
+                    alert( "Data Saved: " + msg );
+                });
+            }
+        });
+    main = new Main({ el : $('body').get(0) });
+    main.render();
+});
