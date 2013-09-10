7513fbd8d5f719b34f997579a864c5a39bd276e9
gremzlya <sinelnikovwork@gmail.com>
2013-09-10 11:15:03 -0700

    update

---
+++ api/models/Words.js
@@ -0,0 +1,21 @@
+/**
+ * Words
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+  attributes: {
+      mainWord: 'string',
+      firstType: 'string'
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	*/
+    
+  }
+
+};
